fa8576624884c0a2ba41e1632df0d322c73cf7b4
sadayasu <yuta.sadayasu@rooter.co.jp>
2020-11-11 15:16:16 +0900

    検索フォームに入力した値を保持するように改修

--- app/controllers/personas_controller.rb
+++ app/controllers/personas_controller.rb
@@ -1,11 +1,12 @@
 class PersonasController < ApplicationController
   def index
-    params[:arcana] = nil if params[:arcana].blank?
-    @personas = Persona.search(params)
-    # binding.pry
+    @search_params = {
+      name: params[:name].blank? ? nil : params[:name],
+      arcana: params[:arcana].blank? ? nil : params[:arcana]
+    }
+    @personas = Persona.search(@search_params)
   end
 
   def show
-    # @persona = Persona.find()
   end
 end
