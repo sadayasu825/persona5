7074e4a16e8e19aa53a938dada9911d574bd9bdc
sadayasu <yuta.sadayasu@rooter.co.jp>
2020-11-18 11:20:50 +0900

    シミュレーター画面追加 サイドバーにリンクを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/' => 'personas#index'
-  get '/personas/:id' => 'personas#show'
+  root to: 'home#index'
+  resources :personas, only: [:index, :show]
+  resources :simulator, only: [:index]
 end
