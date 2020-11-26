1c3b628d1601b39d7f89d8c11864c48c494c0d76
sadayasu <yuta.sadayasu@rooter.co.jp>
2020-11-26 16:48:11 +0900

    rspec導入

--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,15 @@ module Persona5
     # set timezone
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: false,
+        view_specs: false, 
+        helper_specs: false, 
+        controller_specs: false, 
+        routing_specs: false,
+        request_specs: false,
+        model_specs: false,
+    end
   end
 end
